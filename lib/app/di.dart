9b3b63df8f3529358dd8c90eaf7ecad6f3074233
eg-97kr <egor.97kr@gmail.com>
2026-01-09 09:48:50 +0200

    registering dependencies in the service locator

---
+++ lib/app/di.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/widgets.dart';
+import 'package:get_it/get_it.dart';
+import 'package:rick_and_morty_characters/core/network/dio_client.dart';
+import 'package:rick_and_morty_characters/feature/characters/data/repositories/characters_repository_impl.dart';
+import 'package:rick_and_morty_characters/feature/characters/data/sources/characters_api.dart';
+import 'package:rick_and_morty_characters/feature/characters/data/sources/characters_cache.dart';
+import 'package:rick_and_morty_characters/feature/characters/domain/repositories/characters_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  getIt.registerSingleton<SharedPreferences>(
+    await SharedPreferences.getInstance(),
+  );
+
+  getIt.registerLazySingleton<DioClient>(() => DioClient());
+
+  // Data sources
+  getIt.registerLazySingleton<CharactersApi>(
+    () => CharactersApi(getIt<DioClient>().dio),
+  );
+
+  getIt.registerLazySingleton<CharactersCache>(
+    () => CharactersCache(getIt<SharedPreferences>()),
+  );
+
+  // Reposotories
+  getIt.registerLazySingleton<CharactersRepository>(
+    () => CharactersRepositoryImpl(
+      api: getIt<CharactersApi>(),
+      cache: getIt<CharactersCache>(),
+    ),
+  );
+}
